dcf834a5a10e90390c414d0d05d93b7a77e5ce78
Rushikesh Pawar <technify.1nor@gmail.com>
2025-09-20 16:30:44 +0530

    Time: 0 ms (100%), Space: 10.3 MB (79.15%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int left=0, right =0, maxv =0;
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+                left++;
+            else
+            {
+                right++;
+            }
+
+            if(left==right)
+            {
+                maxv = max(maxv, left*2);
+            }
+            if(right>left)
+            {
+                left=0;
+                right =0;
+            }
+        }
+
+        left =0;
+        right =0;
+
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            if(s[i]=='(')
+                left++;
+            else
+            {
+                right++;
+            }
+
+            if(left==right)
+            {
+                maxv = max(maxv, left*2);
+            }
+            if(left>right)
+            {
+                left=0;
+                right =0;
+            }
+        }
+
+        return maxv;
+    }
+
+};
