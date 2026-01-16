24daf5c30b68f99aa0150c68a3646f62b25e1062
Rushikesh Pawar <technify.1nor@gmail.com>
2026-01-16 18:07:14 +0530

    Time: 1 ms (96.13%), Space: 47.3 MB (39.34%) - LeetHub

---
+++ LeetCode/Easy/1929-concatenation-of-array/1929-concatenation-of-array-01-16-2026-18-07-13.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int l = nums.length;
+        int [] ans = new int[l*2];
+        for(int i=0;i<l;i++)
+        {
+            ans[i]=nums[i];
+            ans[i+l]=nums[i];
+        }
+
+        return ans;
+    }
+}
