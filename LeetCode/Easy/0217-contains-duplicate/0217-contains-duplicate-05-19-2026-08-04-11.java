e5e2c12994a99220e11f93e84713e6a5cb785040
Rushikesh Pawar <technify.1nor@gmail.com>
2026-05-19 08:04:12 +0530

    Time: 12 ms (90.28%), Space: 98 MB (6.34%) - LeetHub

---
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate-05-19-2026-08-04-11.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i : nums)
+        {
+            if(!set.add(i))
+            {
+                return true;
+            }
+        }
+
+        return false;
+}
+}
